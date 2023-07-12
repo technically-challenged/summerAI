577f073aec711d5b93f09306f772460b49c63522
Julia Forrester <juliaforrester@Julias-MacBook-Air-4.local>
2023-07-12 14:04:53 -0400

    added task1.py

---
+++ task1.py
@@ -0,0 +1,35 @@
+#hours = input("Enter number of hours")
+#min = 60
+#final = hours * min
+#print(final)
+
+#m = input("enter number of hours")
+#def compute_min(m):
+#    result = m*60
+#    return result
+
+#num_min = compute_min(m)
+
+#print(num_min)
+
+
+def task1():
+    print("convert to min")
+    numb = int(input())
+    numb *= 60
+    print(numb)
+    return numb
+
+
+task1()
+
+#min = 60
+#final = hours * min
+#print(final)
+#def compute_min(m):
+#    result = m*60
+#    return result
+
+#num_min = compute_min(m)
+
+#print(num_min)
